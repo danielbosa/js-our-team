c9c00bb8bcb016b94691ebbb2466343ccba3e4c4
danielbosa <danielbosa95@gmail.com>
2024-03-21 23:32:35 +0100

    Hiring function

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/*
-BONUS 3:
-Permettere l'aggiunta di un nuovo membro del team
-*/
-
 const team = [
     {
         fullname: 'Wayne Barnett',
@@ -56,3 +51,61 @@ for(let key in team){
     `;
     teamStage.append(teamCard);
 };
+
+const newTeam = [
+    {
+        fullname: 'Giovanni Storti',
+        role: 'Pdor figlio di Kmer',
+        pic: 'Pdor-2.png'
+    },
+    {
+        fullname: 'Ajeje Brazorf',
+        role: 'Sprinter',
+        pic: 'Ajeje-2.png'
+    },
+    {
+        fullname: 'Giacomo Poretti',
+        role: 'Tafazzi',
+        pic: 'Tafazzi.gif'
+    }
+];
+
+const teamHired = [];
+
+const btnHire = document.getElementById('btn-hire');
+btnHire.addEventListener('click',hiring);
+
+//const btnFire = document.getElementById('btn-fire');
+//btnFire.addEventListener('click',firing);
+
+const selectEmployee = document.getElementById('hire-menu');
+//selectHire.addEventListener('change',hiring);
+
+function hiring(){
+    if(teamHired.includes(newTeam[selectEmployee.value])){
+        console.log('Employee already hired!');
+        return;
+    };
+    let teamCard = document.createElement('div');
+    teamCard.classList = 'card';
+    teamCard.setAttribute('id',`${newTeam[selectEmployee.value]['fullname']}`)
+    teamCard.innerHTML = `
+        <img src="./img/${newTeam[selectEmployee.value]['pic']}" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">${newTeam[selectEmployee.value]['fullname']} </h5>
+            <p class="card-text">${newTeam[selectEmployee.value]['role']}</p>
+        </div>
+    `;
+    teamStage.append(teamCard);
+    //to avoid having duplicated items, I remove the item selected at change/click
+    teamHired.push(newTeam[selectEmployee.value]);
+    console.log(teamHired);
+    //delete newTeam[selectEmployee.value];//remove item from array
+
+    //let optionSelected = document.querySelector(`#option-${selectEmployee.value}`);//hide option from select
+    //optionSelected.className = 'd-none';
+};
+
+//function firing(){
+    
+//};
